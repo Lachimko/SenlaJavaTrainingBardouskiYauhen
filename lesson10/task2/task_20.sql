0ad599aedaf2c31d96c91a8fc905ea3803771b04
Yauhen Bardouski <makkadessia@gmail.com>
2017-01-26 23:23:57 +0300

    Update task_20.sql

--- lesson10/task2/task_20.sql
+++ lesson10/task2/task_20.sql
@@ -1,4 +1,4 @@
 use senla;
 #20
 select p.maker, count(*) from product as p, pc
-where p.model = pc.model group by p.maker having count(*) > 3;
+where p.model = pc.model group by p.maker having count(*) >= 3;
