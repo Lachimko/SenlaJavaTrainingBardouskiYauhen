6a7b0abd0dca827db449c53856e09fddc2647a1c
makkadessia <makkadessia@gmail.com>
2017-03-24 16:19:26 +0300

    no message

--- lesson16/auto-managment/!Angular/js/controllers/ctrl.js
+++ lesson16/auto-managment/!Angular/js/controllers/ctrl.js
@@ -1,16 +1,18 @@
 angular.module('ctrls', [])
     
-   .controller('MechanicCtrl', function ($scope, $http, cons) {
+   .controller('MechanicCtrl', ['$scope', '$http', 'cons', function ($scope, $http, cons) {
 
     $scope.caption = cons().mechCaption;
     $scope.radioValue = cons().defaultradioValue;
 
     $scope.findId = function () {
-      
+    
+    // $http.get('/someUrl', config).then(successCallback, errorCallback)
+    
         $http({
             method: 'GET',
             params: {id: $scope.id},
-            url: cons().domain + '/MechanicServlet'
+            url: cons().domain + '/MechanicServlet' + '?id=' + $scope.id
         }).then(function (response) {
             $scope.mechanics=[];
             $scope.mechanics.push(response.data);
@@ -44,7 +46,7 @@ angular.module('ctrls', [])
       
        $http({
             method: 'DELETE',
-            params: {id: $scope.id},
+            params: {id: identifier},
             url: cons().domain + '/MechanicServlet'
         }).then(function (response) {
             $scope.resp = response.data;
@@ -52,12 +54,29 @@ angular.module('ctrls', [])
           })
     }
    
-})
-.controller('WorkplaceCtrl', function ($scope, cons) {
+}])
+.controller('WorkplaceCtrl', ['$scope','cons', function ($scope, cons) {
 
     $scope.caption = cons().workCaption;
-})
-.controller('OrderCtrl', function ($scope, cons) {
+
+}])
+.controller('OrderCtrl', ['$scope', '$http', '$log', 'cons', function ($scope, $http, $log, cons) {
 
     $scope.caption = cons().ordCaption;
-});
+    $scope.radioValue = cons().defaultradioValue;
+    $scope.sortReverse = false;
+    $log.info($scope.sortReverse);
+
+    $scope.viewList = function () {
+      
+		$http({
+		    method: 'GET',
+		    params: {sorting: $scope.radioValue},
+		    url: cons().domain + '/TicketListServlet'
+		}).then(function (response) {
+		    $scope.orders = response.data;
+		  })
+	}
+
+}]);
+
--- lesson16/auto-managment/servlet-layer/src/main/java/com/bardouski/runnerservlets/Runner.java
+++ lesson16/auto-managment/servlet-layer/src/main/java/com/bardouski/runnerservlets/Runner.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import com.bardouski.config.ProgramProps;
 import com.bardouski.facade.IFacade;
+import com.bardouski.model.impl.Ticket;
 import com.bardouski.model.impl.dto.MechanicDTO;
+import com.bardouski.model.impl.dto.TicketDTO;
 import com.bardouski.program.facade.Facade;
 import com.bardouski.propertiesholder.PropertiesContext;
 
@@ -18,25 +20,31 @@ public class Runner {
 //	private static final String ALL_KEYWORD = "";
 	
 	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
-
-		Facade facade = null;
-
-		try {
-
-			new PropertiesContext(CONTEXT);
-			ProgramProps programProps = (ProgramProps) PropertiesContext.getInstance(ProgramProps.class);
-			programProps.configure(CONFIG);
-
-			facade = (Facade) PropertiesContext.getInstance(IFacade.class);
-
-			List<MechanicDTO> list = facade.getAllMechanics("currentOrder");
-			for (MechanicDTO mechanicDTO : list) {
-				System.out.println(mechanicDTO);
-			}
-			
-		} catch (ClassNotFoundException | FileNotFoundException e) {
-			e.printStackTrace();
+		
+		
+		Byte i;
+		
+		for (i = 0; i < 100; i++) {
+			System.out.println(i);
 		}
+//		Facade facade = null;
+//
+//		try {
+//
+//			new PropertiesContext(CONTEXT);
+//			ProgramProps programProps = (ProgramProps) PropertiesContext.getInstance(ProgramProps.class);
+//			programProps.configure(CONFIG);
+//
+//			facade = (Facade) PropertiesContext.getInstance(IFacade.class);
+////
+////			List<TicketDTO> list = facade.getAllMechanics("currentOrder");
+////			for (MechanicDTO mechanicDTO : list) {
+////				System.out.println(mechanicDTO);
+////			}
+			
+//		} catch (ClassNotFoundException | FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
